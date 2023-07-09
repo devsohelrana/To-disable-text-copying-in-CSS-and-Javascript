89fd36cc0522709e00b9dad28a87a9452baa48df
devsohelrana <dev.sohelrana@gmail.com>
2023-07-09 23:41:10 +0600

    To disable text copying in CSS and Javascript

---
+++ app.js
@@ -0,0 +1,16 @@
+// Add event listener for copy event
+document.addEventListener('copy', function (event) {
+    event.preventDefault(); // Prevent default copy behavior
+
+    // Toast element setect using id
+    var toast = document.getElementById('toast');
+    // Show toast message
+    toast.textContent = 'Copying content is not allowed.';
+    // Toast element add class
+    toast.classList.add('show');
+
+    // Remove toast after a certain time (e.g., 2 seconds)
+    setTimeout(function () {
+        toast.classList.remove('show');
+    }, 2000);
+});
